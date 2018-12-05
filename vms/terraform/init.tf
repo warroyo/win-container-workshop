5e8936aaff8efbb35e211016185bd3c7a38d24cd
warroyo <warroyo7199008@gmail.com>
2018-12-05 10:49:31 -0700

    add remote tf state

--- vms/terraform/init.tf
+++ vms/terraform/init.tf
@@ -1,6 +1,15 @@
 # Configure the Microsoft Azure Provider
 provider "azurerm" {}
 
+terraform {
+  backend "azurerm" {
+    resource_group_name = "terraform-state"
+    storage_account_name = "watfstate"
+    container_name       = "win-workshop-tfstate"
+    key                  = "winworkshop.terraform.tfstate"
+  }
+}
+
 # Create a resource group
 resource "azurerm_resource_group" "global" {
   location = "${var.location}"
@@ -15,6 +24,8 @@ resource "azurerm_storage_account" "global" {
   name                     = "${var.account}"
   resource_group_name      = "${azurerm_resource_group.global.name}"
 }
+
+
 
 data "azurerm_image" "workshop_image" {
   name                = "${var.workshop_image}"
